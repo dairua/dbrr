76c9c7b995545287c53e990b7c25b273cf39b054
dairua <phamdai2032k1@gmail.com>
2023-05-08 14:22:39 +0700

    'dbrr'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,29 @@
+import { DialogAllModule } from '@syncfusion/ej2-angular-popups';
+
+import { AccumulationChartModule } from '@syncfusion/ej2-angular-charts';
+
+import { ChartAllModule } from '@syncfusion/ej2-angular-charts';
+import { DiagramAllModule, SymbolPaletteAllModule, OverviewAllModule } from '@syncfusion/ej2-angular-diagrams';
+
+import { GridAllModule } from '@syncfusion/ej2-angular-grids';
+
+import { ListViewAllModule } from '@syncfusion/ej2-angular-lists';
+
+import { DateRangePickerModule } from '@syncfusion/ej2-angular-calendars';
+
+import { CircularGaugeModule } from '@syncfusion/ej2-angular-circulargauge';
+
+import { DropDownListAllModule } from '@syncfusion/ej2-angular-dropdowns';
+
+import { MultiSelectModule } from '@syncfusion/ej2-angular-dropdowns';
+
+import { ToolbarModule } from '@syncfusion/ej2-angular-navigations';
+
+import { NumericTextBoxModule, ColorPickerModule, UploaderModule, TextBoxModule } from '@syncfusion/ej2-angular-inputs';
+
+import { DropDownButtonModule } from '@syncfusion/ej2-angular-splitbuttons';
+
+import { ButtonModule, CheckBoxModule, RadioButtonModule } from '@syncfusion/ej2-angular-buttons';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -9,6 +35,29 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    DiagramAllModule, 
+    ChartAllModule, 
+    GridAllModule, 
+    SymbolPaletteAllModule, 
+    OverviewAllModule, 
+    ButtonModule, 
+    ColorPickerModule, 
+    DateRangePickerModule, 
+    CheckBoxModule, 
+    AccumulationChartModule, 
+    BrowserModule,
+    ToolbarModule, 
+    DropDownButtonModule, 
+    UploaderModule, 
+    CircularGaugeModule, 
+    DropDownListAllModule, 
+    ListViewAllModule, 
+    DialogAllModule, 
+    TextBoxModule, 
+    RadioButtonModule, 
+    MultiSelectModule, 
+    NumericTextBoxModule, 
+    BrowserModule,
     BrowserModule,
     AppRoutingModule
   ],
